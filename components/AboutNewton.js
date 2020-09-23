df76b35b55296947431c793412cd27c73a473c72
arisac <arisac@tomsc.com>
2020-09-23 00:58:37 -0400

    update content and ui

--- components/AboutNewton.js
+++ components/AboutNewton.js
@@ -5,17 +5,19 @@ import { useTranslation, Trans, Translation } from 'react-i18next'
 const AboutNewton = () => {
   let { t ,i18n} = useTranslation();
   return (
-    <section className="pt-50 pb-50">
+    <section className="pt-0 pb-30 about-newton">
       <Container>
         <Row className="justify-content-center">
         <Col md={10} lg={10} xl={10}>
             <div className="text-center">
               <p>{t('newton.brought')}</p>
-              <h3 className="h1">
+              <h3 style={{
+                marginBottom: 30,
+              }}>
                 {t('newton.base')}
               </h3>
               <h4 style={{
-                marginBottom: 25,
+                marginBottom: 30,
               }}>
                 {t('newton.slogan')}
               </h4>
--- components/HomeSections.js
+++ components/HomeSections.js
@@ -2,23 +2,19 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { useTranslation, Trans, Translation } from 'react-i18next'
 
-import img1 from "../assets/img/feature/user-interact.png";
-
 
 const HomeSections = () => {
   let { t ,i18n} = useTranslation();
   return (
     <>
-      <section className="-border-top pt-60 pb-60 bg-2- ">
+      <section className="-border-top pt-60 pb-30 bg-2- ">
         <Container>
           <Row className="justify-content-center">
-            <Col md={12} lg={10}>
-              <div className="text-center">
-                <h2>
-                  经济模型
-                </h2>
-                <p>NewSwap 中的通证价格由系统定价，在 NewSwap 中发生的所有交易将收取 0.3% 的交易手续费，所有的手续费都提供给协议的流动性提供者，交易平台不收取任何手续费。</p>
-                <p>其中，交易中的 0.25% 将直接分配给流动性提供者，0.05% 将进入流动性收益池，以流动性通证的形式支撑收益代币NewSwap Token(NST) 的价值。</p>
+            <Col md={10} lg={10}>
+              <div className="text-center user-interact-inner">
+                <h2>{t('business.title')}</h2>
+                <p>{t('business.text1')}</p>
+                <p>{t('business.text2')}</p>
                 <p><a href="#">{t('global.learn')}</a></p>
               </div>
             </Col>
