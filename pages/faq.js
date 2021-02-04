7a4f15c0bdfbdd6fbe8e1875437ad7cd8509bee7
weixuefeng <weixuefeng1018@gmail.com>
2021-02-04 13:48:25 +0800

    Revert "fix change language bug"
    This reverts commit e27b29ecb14154235d15fd218ddaae539fa27e93.

--- pages/faq.js
+++ pages/faq.js
@@ -13,20 +13,39 @@ import remark2react from "remark-react";
 import markdown_en from "../faq/faq.en.md";
 import markdown_zh from "../faq/faq.zh.md";
 
-import { useTranslation, Trans, Translation } from "react-i18next";
-
-const content_en = unified()
-  .use(parse)
-  .use(remark2react)
-  .processSync(markdown_en).result;
-
-const content_zh = unified()
-  .use(parse)
-  .use(remark2react)
-  .processSync(markdown_zh).result;
+import { useTranslation } from "react-i18next";
+import { useState, useEffect } from "react"
 
 const PostDetails = () => {
-  let { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const content_en = unified()
+    .use(parse)
+    .use(remark2react)
+    .processSync(markdown_en).result;
+
+  const content_zh = unified()
+    .use(parse)
+    .use(remark2react)
+    .processSync(markdown_zh).result;
+
+  const getLanguage = () => {
+    let language = i18n.language
+    let languageCss;
+    if(language.indexOf("en") > -1) {
+      languageCss ="content-en"
+    } else {
+      languageCss = "content-zh"
+    }
+    return languageCss;
+  }
+
+  const [language, setlanguage] = useState("content-en")
+
+  useEffect(() => {
+    setlanguage(getLanguage());
+  }, [i18n.language])
+
   return (
     <Layout pageTitle={t("pages.faq") + " - " + t("global.site title")}>
       <Header />
@@ -37,7 +56,7 @@ const PostDetails = () => {
             <Col lg={9}>
               <div className="post-details-inner">
                 <div className="single-post-inner">
-                  <div className={"post-content " + t("global.content lang")}>
+                  <div className={"post-content " + language}>
                     <div className="post-details">
                       <div className="post-title">
                         <h3>{t("global.faq")}</h3>
