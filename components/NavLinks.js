d54262757a84b4b3467bbc9e32e0c9fbaf57466a
arisac <hi@aris.ac>
2021-02-18 20:33:51 -0500

    patch: navlinks mining testnet/mainnet

--- components/NavLinks.js
+++ components/NavLinks.js
@@ -19,32 +19,7 @@ const NavLinks = () => {
         </Link>
       </li>
 
-      <li className="dropdown">
-        <Link href="#">
-          <>
-            <a href="#" onClick={handleDropdownStatus}>
-              {t("global.mining")}
-            </a>
-            <i className="fa fa-angle-down"></i>
-          </>
-        </Link>
-        <ul className="dropdown-list">
-          <li>
-            <Link href={process.env.NEXT_PUBLIC_NEWSWAP_MINING_URL}>
-              <a>{t("mining.nusdt mining")}</a>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href={
-                process.env.NEXT_PUBLIC_NEWSWAP_MINING_URL + "/communityMining"
-              }
-            >
-              <a>{t("mining.community mining")}</a>
-            </Link>
-          </li>
-        </ul>
-      </li>
+      <NavLinksMiningPatch />
 
       <li href="#" className="dropdown">
         <Link href="#">
@@ -151,3 +126,50 @@ const NavLinks = () => {
 };
 
 export default NavLinks;
+
+const NavLinksMiningPatch = () => {
+  const handleDropdownStatus = (e) => {
+    let clickedItem = e.currentTarget.parentNode;
+    clickedItem.querySelector(".dropdown-list").classList.toggle("show");
+  };
+  let { t, i18n } = useTranslation();
+
+  if (process.env.NEXT_PUBLIC_ENV_NOTICE == "") {
+    return (
+      <li>
+        <Link href={process.env.NEXT_PUBLIC_NEWSWAP_MINING_URL}>
+          <a>{t("global.mining")}</a>
+        </Link>
+      </li>
+    );
+  } else {
+    return (
+      <li className="dropdown">
+        <Link href="#">
+          <>
+            <a href="#" onClick={handleDropdownStatus}>
+              {t("global.mining")}
+            </a>
+            <i className="fa fa-angle-down"></i>
+          </>
+        </Link>
+        <ul className="dropdown-list">
+          <li>
+            <Link href={process.env.NEXT_PUBLIC_NEWSWAP_MINING_URL}>
+              <a>{t("mining.nusdt mining")}</a>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={
+                process.env.NEXT_PUBLIC_NEWSWAP_MINING_URL + "/communityMining"
+              }
+            >
+              <a>{t("mining.community mining")}</a>
+            </Link>
+          </li>
+        </ul>
+      </li>
+    );
+  }
+};
